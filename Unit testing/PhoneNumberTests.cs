8d3c774186dd22a7a61e397de3e4616cfb4e1d66
Playdovsky <144317216+Playdovsky@users.noreply.github.com>
2024-04-02 14:33:27 +0200

    Preparation of unit testing + phone number unit tests

---
+++ Unit testing/PhoneNumberTests.cs
@@ -0,0 +1,67 @@
+namespace Unit_testing
+{
+    public class PhoneNumberTests
+    {
+        [Fact]
+        public void GoodNumber1()
+        {
+            string phoneNumber = "752-634-879";
+
+            Assert.True(Check(phoneNumber));
+        }
+
+        [Fact]
+        public void GoodNumber2()
+        {
+            string phoneNumber = "655 438 671";
+
+            Assert.True(Check(phoneNumber));
+        }
+
+        [Fact]
+        public void GoodNumber3()
+        {
+            string phoneNumber = "512733775";
+
+            Assert.True(Check(phoneNumber));
+        }
+
+        [Fact]
+        public void BadNumber1()
+        {
+            string phoneNumber = "74256762";
+
+            Assert.False(Check(phoneNumber));
+        }
+
+        [Fact]
+        public void BadNumber2()
+        {
+            string phoneNumber = "678 723 3s1";
+
+            Assert.False(Check(phoneNumber));
+        }
+
+        [Fact]
+        public void BadNumber3()
+        {
+            string phoneNumber = "777-642-5618";
+
+            Assert.False(Check(phoneNumber));
+        }
+
+        public static bool Check(string phoneNumber)
+        {
+            phoneNumber = phoneNumber.Replace(" ", "").Replace("-", "");
+
+            if (phoneNumber.Length == 9 && phoneNumber.All(Char.IsDigit))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
